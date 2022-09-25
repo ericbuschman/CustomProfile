c1031c0157d3f460e5f5237b692e7aee277fc852
Eric Buschman <eric.buschman@gmail.com>
2022-09-25 07:52:03 -0700

    Git merged branches cleanup

---
+++ Profile-Scripts/Function-Remove-MergedBranches.ps1
@@ -0,0 +1,7 @@
+function Remove-MergedBranches {
+    git branch --merged |
+    ForEach-Object { $_.Trim() } |
+    Where-Object { $_ -NotMatch "^\*" } |
+    Where-Object { -not ( $_ -Like "*master" -or $_ -Like "*main" -or $_ -Like "*dev" ) } |
+    ForEach-Object { git branch -d $_ }
+}
