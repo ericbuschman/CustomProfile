a5514765a37c83ca9b30084ab9dd617c9cb01020
Eric Buschman <eric.buschman@gmail.com>
2022-10-05 08:26:52 -0700

    Better breadcrumbs and folder structure

--- Profile-Scripts/Function-New-Daily.ps1
+++ Profile-Scripts/Function-New-Daily.ps1
@@ -16,32 +16,63 @@
 Function New-DailyNote (
     $notesDirectory = "$([Environment]::GetFolderPath("MyDocuments"))\Daily Notes"
 ) {
+    $currentPwd = $PWD
     if (-Not (Test-Path -Path $notesDirectory)) { mkdir $notesDirectory }
-    Push-Location $notesDirectory
+    Set-Location $notesDirectory
 
-    $todaysFile = ".\$(Get-Date -f "yyyy-MM-dd").md"
-    $previousFile = ".\$(Split-Path ((Get-ChildItem .\*.md -Exclude _template.md | Sort-Object CreationTime)[-1]) -Leaf)"
+    # Write readme file
+    if (-Not (Test-Path -Path .\readme.md)) {
+        '# Daily notes
+The files are laid out in a year \ month structure to keep the root of the tree from having an overwhelming number of files
+
+Example:
+```
+.\DAILY NOTES
+└───2022
+    ├───09
+    └───10
+```
+'  | Set-Content -Path .\readme.md
+    }
+
+    # Set up variables
+
+    $todaysPath = ".\$(Get-Date -f "yyyy\\MM")"
+    $previousPath = ".\$((get-date).AddDays(-1).ToString("yyyy\\MM"))"
+
+    $todaysFile = "$(Get-Date -f "yyyy-MM-dd").md"
+
+    Push-Location $previousPath
+
+    $previousFile = "$(((Get-ChildItem .\*.md -Exclude _template.md | Sort-Object CreationTime)[-1]).FullName)"
     $catPreviousFile = (Get-Content -Path $previousFile)
+
+    Pop-Location
+
+    Push-Location $todaysPath
 
     # Template markdown definition
     $template = "
-[Previous]($($previousFile)) | {{Next}}
+[Previous]($(Resolve-Path -Path $previousFile -Relative)) | {{Next}}
 
 # $(Get-Date)
 
-## Tasks
-"
+## Tasks"
 
     # Let's carry forward the not completed tasks from yesterday
     foreach ($line in $catPreviousFile) {
         if ($line.startsWith("## Tasks")) {
             $inSection = $true
+            Write-Host "Found Tasks"
+            continue
         }
-        if ($inSection -and $line.startsWith("- [ ]")) {
-            $template += $line
+        if ($inSection -and $line -match "^- \[ \] \w") {
+            $template += "`r`n" + $line 
+            Write-Host "Adding Line"
         }
         # If we get to the next section, stop parsing
         if ($inSection -and $line.StartsWith("##")) {
+            Write-Host "All done"
             break
         }
     }
@@ -61,18 +92,19 @@ Function New-DailyNote (
 ## Communication Log
 "
 
-    if (-Not (Test-Path $todaysFile)) {
-        Set-Content -Path $todaysFile -Value $template
+    if (-Not (Test-Path .\$todaysFile)) {
+        Set-Content -Path .\$todaysFile -Value $template
     }
-
-    $catPreviousFile.replace("{{Next}}", "[Next]($todaysFile)") | Set-Content -Path $previousFile -Force
-
-    # TODO: build out an archival process for old notes
-    # Archiving with updating breadcrumbs
-
-    
-
-    Code $todaysFile
+    $todaysFileLoc = (Get-ChildItem -Path .\$todaysFile).Fullname
 
     Pop-Location
+    Push-Location $previousPath
+
+    $catPreviousFile.replace("{{Next}}", "[Next]($(Resolve-Path -Path $todaysFileLoc -Relative))") | Set-Content -Path $previousFile -Force
+
+    Pop-Location
+
+    Code $todaysFileLoc
+
+    Set-Location $currentPwd
 }
